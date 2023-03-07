a28a4769ee1864b4790d8b4ce4c1128138e52168
Ryan Baer <ryanjbaer@outlook.com>
2023-03-07 09:54:44 -0700

    fix input names and create new url function

--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -6,7 +6,7 @@ class UrlForm extends Component {
     this.props = props;
     this.state = {
       title: '',
-      urlToShorten: ''
+      long_url: ''
     };
   }
 
@@ -16,11 +16,17 @@ class UrlForm extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    const urlsLength = this.props.urls.length
+    const newUrl = {
+      id: urlsLength + 1,
+      ...this.state
+    }
+    this.props.sendUrl(newUrl)
     this.clearInputs();
   }
 
   clearInputs = () => {
-    this.setState({title: '', urlToShorten: ''});
+    this.setState({title: '', long_url: ''});
   }
 
   render() {
@@ -37,8 +43,8 @@ class UrlForm extends Component {
         <input
           type='text'
           placeholder='URL to Shorten...'
-          name='title'
-          value={this.state.title}
+          name='long_url'
+          value={this.state.long_url}
           onChange={e => this.handleNameChange(e)}
         />
 
